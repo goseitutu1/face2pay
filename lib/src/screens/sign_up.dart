3d6ac3fd0d34ecf8847f327f197fa7dbc4e70976
goseitutu1 <oseitutugideon1@gmail.com>
2020-07-06 11:23:32 +0100

    commit

--- lib/src/screens/sign_up.dart
+++ lib/src/screens/sign_up.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:async';
 import 'dart:io';
 import '../blocs/sign_up_bloc.dart';
@@ -18,6 +19,12 @@ class SignUp extends StatefulWidget {
 class SignUpState extends State<SignUp> {
 
   final picker = ImagePicker();
+  bool  _imageUploaded = false;
+  String _downloadImageURL;
+  bool _showSpinner = false; 
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  StorageReference _reference = FirebaseStorage.instance.ref().child(signUpBloc.regImage.toString());
+
   Future _getImage() async {
     var _pickedImage = await picker.getImage(source: ImageSource.camera);
     
@@ -25,13 +32,28 @@ class SignUpState extends State<SignUp> {
       signUpBloc.regImage = File(_pickedImage.path);
       print('image: $signUpBloc.regImage');
     });
-    
     // _image;
     // String _email = signUpBloc.getEmail();
   }
 
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-  bool _showSpinner = false; 
+  Future _uploadImage() async {
+    StorageUploadTask uploadTask = _reference.putFile(signUpBloc.regImage);
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+
+    setState(() {
+      _imageUploaded = true;
+    });
+    print('taskSnapshot: $taskSnapshot');
+  }
+
+  Future _downloadImageURL() async {
+    String _downloadAddress = await _reference.getDownloadURL();
+
+    setState(() {
+      _downloadImageURL = _downloadAddress;
+    });
+  }
+
   Widget build(BuildContext context){
     return Scaffold(
       key: _scaffoldKey,
@@ -50,7 +72,10 @@ class SignUpState extends State<SignUp> {
                     width: 300.0,
                     height: 150.0,
                     decoration: BoxDecoration(
-                      image: DecorationImage(image: AssetImage('images/logo.png'), fit: BoxFit.fill),),
+                      image: DecorationImage(
+                        image: AssetImage('images/logo.png'), 
+                        fit: BoxFit.fill),
+                      ),
                   ),
                   ListTile(
                   title: Text('Sign Up',
@@ -73,64 +98,70 @@ class SignUpState extends State<SignUp> {
                   SizedBox(height: 20.0),
                   ListTile(
                     onTap:  _getImage,
-                      // Navigator.pushNamed(context, '/registerface');
                     title: FacialIcon(description: 'Register face'),
                   ),
                   SizedBox(height: 10.0),
                   Divider(thickness: 1.0),
+                  signUpBloc.regImage == null ? Container() :
                   Button(
                     description: 'Sign up',
                     onPressed: () async {
-                      String _email = signUpBloc.getEmail();
-                      String _password = signUpBloc.getPassword();
-                      String _confirmPassword = signUpBloc.getConfirmPassword();
-                      if((_email.contains('@'))&&(_password.length >= 6)&&(_confirmPassword.length >= 6)&&(_password==_confirmPassword)) {
-                        setState(() {
-                          _showSpinner = true;
-                        });
-                        try{
-                          final _auth = FirebaseAuth.instance;
-                          final response = await _auth.createUserWithEmailAndPassword(email: _email, password: _password);
-                          if(response != null){
-                            Navigator.pushNamed(context, '/home');
-                          }
-                          setState((){
-                            _showSpinner = false;
-                          });
-                        }
-                        catch(e){
-                          setState((){
-                            _showSpinner = false;
-                          });
-                          String errorMessage = e.toString().substring(24,44);
-                          _scaffoldKey.currentState.showSnackBar(
-                            SnackBar(
-                              backgroundColor: Colors.red,
-                              content: Text(
-                                '$errorMessage',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          );
-                        }
-                      }
-                      else{
-                        _scaffoldKey.currentState.showSnackBar(
-                          SnackBar(
-                            backgroundColor: Colors.red,
-                            content: Text(
-                              'invalid inputs',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        );
-                      }
+                      setState(() {
+                        _showSpinner = true;
+                      });
+                      _uploadImage();
+                      _downloadImageURL();
+
+                    //   String _email = signUpBloc.getEmail();
+                    //   String _password = signUpBloc.getPassword();
+                    //   String _confirmPassword = signUpBloc.getConfirmPassword();
+                    //   if((_email.contains('@'))&&(_password.length >= 6)&&(_confirmPassword.length >= 6)&&(_password==_confirmPassword)) {
+                    //     setState(() {
+                    //       _showSpinner = true;
+                    //     });
+                    //     try{
+                    //       final _auth = FirebaseAuth.instance;
+                    //       final response = await _auth.createUserWithEmailAndPassword(email: _email, password: _password);
+                    //       if(response != null){
+                    //         Navigator.pushNamed(context, '/home');
+                    //       }
+                    //       setState((){
+                    //         _showSpinner = false;
+                    //       });
+                    //     }
+                    //     catch(e){
+                    //       setState((){
+                    //         _showSpinner = false;
+                    //       });
+                    //       String errorMessage = e.toString().substring(24,44);
+                    //       _scaffoldKey.currentState.showSnackBar(
+                    //         SnackBar(
+                    //           backgroundColor: Colors.red,
+                    //           content: Text(
+                    //             '$errorMessage',
+                    //             style: TextStyle(
+                    //               color: Colors.black,
+                    //               fontWeight: FontWeight.bold,
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       );
+                    //     }
+                    //   }
+                    //   else{
+                    //     _scaffoldKey.currentState.showSnackBar(
+                    //       SnackBar(
+                    //         backgroundColor: Colors.red,
+                    //         content: Text(
+                    //           'invalid inputs',
+                    //           style: TextStyle(
+                    //             color: Colors.black,
+                    //             fontWeight: FontWeight.bold,
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     );
+                    //   }
                     },
                   ),
                 ],
@@ -146,7 +177,13 @@ class SignUpState extends State<SignUp> {
     return StreamBuilder(
       stream: signUpBloc.emailStream,
       builder: (context, AsyncSnapshot<String> snapshot) {
-        return TextFeild(onChanged: signUpBloc.changeEmail,errorText: snapshot.error,obscureText: false,hintText: 'Email',icon: Icon(Icons.email));
+        return TextFeild(
+          onChanged: signUpBloc.changeEmail,
+          errorText: snapshot.error,
+          obscureText: false,
+          hintText: 'Email',
+          icon: Icon(Icons.email)
+        );
       } 
     );
   }
@@ -155,7 +192,13 @@ class SignUpState extends State<SignUp> {
      return StreamBuilder(
       stream: signUpBloc.passwordStream,
       builder: (context, AsyncSnapshot<String> snapshot) {
-        return TextFeild(onChanged: signUpBloc.changePassword,errorText: snapshot.error,obscureText: true,hintText: 'Password',icon: Icon(Icons.enhanced_encryption));
+        return TextFeild(
+          onChanged: signUpBloc.changePassword,
+          errorText: snapshot.error,
+          obscureText: true,
+          hintText: 'Password',
+          icon: Icon(Icons.enhanced_encryption)
+        );
       }
     );
   }
@@ -164,7 +207,13 @@ class SignUpState extends State<SignUp> {
      return StreamBuilder(
       stream: signUpBloc.confirmPasswordStream,
       builder: (context, AsyncSnapshot<String> snapshot) {
-        return TextFeild(onChanged: signUpBloc.changeConfirmPassword,errorText: snapshot.error,obscureText: true,hintText: 'Confirm Password',icon: Icon(Icons.enhanced_encryption));
+        return TextFeild(
+          onChanged: signUpBloc.changeConfirmPassword,
+          errorText: snapshot.error,
+          obscureText: true,
+          hintText: 'Confirm Password',
+          icon: Icon(Icons.enhanced_encryption)
+        );
       }
     );
   }
